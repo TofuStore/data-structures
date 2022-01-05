23d8c7ba127658957432ce402f9dd0b1812951b3
Trevor Leung <humormy@gmail.com>
2022-01-05 15:29:30 -0800

    Finished functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {storage: {}};
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
+stackMethods.push = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
+
+stackMethods.pop = function() {
+  let popped = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return popped;
+};
