9e0087535b8adef5e74ab8affcf99294169798a5
Trevor Leung <humormy@gmail.com>
2022-01-05 15:55:37 -0800

    Finished pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[Object.keys(this.storage)[Object.keys(this.storage).length - 1] + 1] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  let dequeued = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return dequeued;
 };
 
 
