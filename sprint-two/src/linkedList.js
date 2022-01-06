dbe0303d3cda4b217c4a9fcb34a6aac2d1f8be3c
Skipper Harris <skipdawg3000@gmail.com>
2022-01-06 11:31:56 -0800

    passing linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail) {
+      list.tail.next = Node(value);
+    }
+    list.tail = Node(value);
+    if (!list.head) {
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    let lastHead = list.head.value;
+    list.head = list.head.next;
+    return lastHead;
   };
 
   list.contains = function(target) {
+    let targetFound = false;
+    let foundInNode = function(node, target) {
+      if (node.value === target) {
+        targetFound = true;
+      }
+      if (node.next) {
+        foundInNode(node.next, target);
+      }
+    };
+    foundInNode(list.head, target);
+
+    return targetFound;
   };
 
   return list;
