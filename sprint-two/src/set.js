ca73a2f7e41f94aa4e9e0386cd6e017c82f7b53a
Skipper Harris <skipdawg3000@gmail.com>
2022-01-06 17:05:34 -0800

    passing set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this.storage[Object.keys(this.storage)[Object.keys(this.storage).length - 1] + 1] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  let isPresent = false;
+  for (let box in this.storage) {
+    if (this.storage[box] === item) {
+      isPresent = true;
+    }
+  }
+
+  return isPresent;
 };
 
 setPrototype.remove = function(item) {
+  for (let box in this.storage) {
+    if (this.storage[box] === item) {
+      delete this.storage[box];
+    }
+  }
 };
 
 /*
