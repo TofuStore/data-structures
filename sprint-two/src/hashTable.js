25dc2f08de9cebdfba48141ac9de2ba241ac2e64
Skipper Harris <skipdawg3000@gmail.com>
2022-01-07 11:45:30 -0800

    passing hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,42 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // if (typeof(this._storage.get(index)) === 'string') {
-  //   index = getIndexBelowMaxForKey(index, this._limit);
-  // }
-  this._storage.set(index, v);
+  if (this._storage.get(index) !== undefined) {
+    let isPresent = false;
+    for (let i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        this._storage.get(index)[i][1] = v;
+        isPresent = true;
+      }
+    }
+    if (!isPresent) {
+      this._storage.set(index, this._storage.get(index).concat([[k, v]]));
+    }
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
+
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  for (let i = 0; i < this._storage.get(index).length; i++) {
+
+    if (this._storage.get(index)[i][0] === k) {
+      return this._storage.get(index)[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  //this._storage.set(index, undefined);
+  for (let i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index)[i][1] = undefined;
+    }
+  }
 };
 
 
