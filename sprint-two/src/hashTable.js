2eccea7c54cfed67cbf593cf79b91511587b3cfe
Skipper Harris <skipdawg3000@gmail.com>
2022-01-07 10:20:22 -0800

    end of day, working in hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,20 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if (typeof(this._storage.get(index)) === 'string') {
+  //   index = getIndexBelowMaxForKey(index, this._limit);
+  // }
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
